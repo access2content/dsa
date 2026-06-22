9d1957dca0828425594abd7a9fe2cd5416f04929
Vivek Agrawal <access2content@gmail.com>
2026-06-22 16:19:16 +0530

    feat(binary-search): 278 - first-bad-version

---
+++ phase3/1.binary-search/278-first-bad-version.ts
@@ -0,0 +1,25 @@
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function (isBadVersion: any) {
+  return function (n: number): number {
+    let start = 1,
+      end = n;
+
+    while (start <= end) {
+      const mid = start + Math.floor((end - start) / 2);
+
+      if (isBadVersion(mid)) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
+      }
+    }
+
+    return start;
+  };
+};
